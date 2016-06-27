9db7602ea245d82f6f13212414799fc8170bd19b
ro-double-b <robb.marsee@gmail.com>
2016-06-27 15:45:52 -0700

    "Server handles GET Requests"

--- server/request-handler.js
+++ server/request-handler.js
@@ -35,15 +35,18 @@ var requestHandler = function(request, response) {
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
+
+
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
   response.writeHead(statusCode, headers);
+
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +55,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  response.end(JSON.stringify({results: [] }));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -70,4 +73,4 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
-exports.requestHandler =  requestHandler;
+exports.requestHandler = requestHandler;
