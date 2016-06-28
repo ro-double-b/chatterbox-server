585336137cb76c10e10f1ef205d49ddf09fcda94
ro-double-b <robb.marsee@gmail.com>
2016-06-28 15:45:03 -0700

    Refactored Sever Code

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,46 +12,27 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var defaultCorsHeaders = {
+  'Content-Type' : 'application/json',
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
 var stubMsg = {results: []};
 
 
 var requestHandler = function(request, response) {
   var method = request.method;
   var url = request.url;
-  // body = body || [];
-  // var responseBody = {
-  //   headers: headers,
-  //   method: method,
-  //   url: url,
-  //   results: body,
 
-  // };
-  var responseBody;
-
-  // resultObj = resultObj || {
-  //   results: []
-  // };
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
   var statusCode = 404;
-  // Do some basic logging.
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'application/json';
 
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + method + ' for url ' + url);
   
   if (method === 'OPTIONS') {
     statusCode = 200;
-    response.writeHead(statusCode, headers);
+    response.writeHead(statusCode, defaultCorsHeaders);
     response.end();
     
  
@@ -66,55 +47,17 @@ var requestHandler = function(request, response) {
         stubMsg.results.push(JSON.parse(chunk.toString()));
       });
       request.on('end', function() {
-        response.writeHead(statusCode, headers);
+        response.writeHead(statusCode, defaultCorsHeaders);
         response.end();
       });
     }
   } 
-    // console.log(request._postData);
-    // resultObj.results.push(request._postData);
-  // if (request.url !== '/classes/messages') {
-  //   statusCode = 404;
-  // }
-  // See the note below about CORS headers.
-  
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+   
+  response.writeHead(statusCode, defaultCorsHeaders);
 
  
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  // console.log(resultObj);
   response.end(JSON.stringify(stubMsg));
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+
 exports.requestHandler = requestHandler;
